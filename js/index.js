f1f34d921fe14234dffae74225a393ea65db7a6c
Corv1z <adrianjay_20@yahoo.com>
2026-02-18 18:11:51 +0800

    more updateery thignies

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 var icon = document.getElementById("icon");
 var body = document.body;
+var headerImage = document.querySelector(".header-image");
 
 if (localStorage.getItem("darkMode") === "true") {
     body.classList.add("dark-theme");
@@ -18,4 +19,18 @@ function playSound() {
     newAudio.play();
 }
 
+function checkMobileAnimation() {
+    if (window.innerWidth <= 768) {
+        headerImage.classList.add("mobile-active");
+        headerImage.onclick = function() {
+            headerImage.classList.toggle("mobile-active");
+        };
+        
+    } else {
+        headerImage.classList.remove("mobile-active");
+        headerImage.onclick = null;
+    }
+}
 
+checkMobileAnimation();
+window.addEventListener('resize', checkMobileAnimation);
